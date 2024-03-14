79918ed919604415fcb294b771df7af6ee00bfcc
Deepikagowtham <126891628+Deepikagowtham@users.noreply.github.com>
2024-03-14 19:08:48 +0530

    Update Game.java

--- project/codes/Game.java
+++ project/codes/Game.java
@@ -1,21 +1,23 @@
 package game;
-import  board.Board;
+
+import board.Board;
+import java.util.Scanner;
 import player.Player;
-import java.util.*;
 
 public class Game {
-    Player[] players;
+    Player [] players;
     Board board;
     int turn;
     int noOfMoves;
     boolean gameOver;
     String zero;
-    String  cross;
+    String cross;
 
-    public Game(Player[] players, Board board){
+
+    public Game(Player[] players, Board board) {
         this.players = players;
-        this.board=board;
-        this.turn=0;
+        this.board = board;
+        this.turn = 0;
         this.noOfMoves = 0;
         this.gameOver = false;
 
@@ -23,140 +25,155 @@ public class Game {
         StringBuilder c = new StringBuilder();
 
         for(int i=0;i<board.size;i++) {
-            z.append('0');
+            z.append('O');
             c.append('X');
         }
 
         this.zero = z.toString();
         this.cross = c.toString();
+
     }
 
     public void printBoardConfig() {
         int sz = this.board.size;
         for(int i=0;i<sz;i++) {
             for(int j=0;j<sz;j++) {
-                System.out.println(board.matrix[i][j] + " ");
+                System.out.print(board.matrix[i][j] + " ");
             }
             System.out.println();
         }
-    }    
+    }
 
     public void play() {
         printBoardConfig();
         int sz = board.size;
+
         while(!gameOver) {
             noOfMoves++;
-            int index = getIndex();
-            int row = index/sz;
-            int col = index%sz;
+            int idx = getIndex();
 
-            board.matrix[row][col]=players[turn].getPlayerSymbol();
+            int row = idx/sz;
+            int col = idx%sz;
 
-            if(noOfMoves>=sz*sz){
-                System.out.println("Game draw");
-                return ;
+            board.matrix[row][col] = players[turn].getPlayerSymbol();
+
+            if (noOfMoves >= sz*sz) {
+                System.out.println("Game Draw");
+                return;
             }
 
-            if(noOfMoves>=sz*sz && checkCombination()== true) {
+            if (noOfMoves >= 2*sz-1 && checkCombinations() == true) {
                 gameOver = true;
                 printBoardConfig();
-                System.out.println("Winner is "+ players[turn].getPlayerName());
-                return ;
-            }
-        
-            turn = (turn+1)%players.length;
+                System.out.println("Winner is: " + players[turn].getPlayerName());
+                return;
+            } 
+            
+            // turn  = 0 => 0+1 = 1%2 = 1;
+            // turn = 1 => 1+1 = 2%2 = 0;
+            // if (turn == 0) turn = 1;
+            // else turn = 0;
+
+            turn  = (turn +1)%2;
             printBoardConfig();
-
-
 
         }
 
     }
 
-    
     public int getIndex() {
         while(true) {
-            System.out.println("Player: " + players[turn].getPlayerName() + " give one postion");
-            Scanner sc = new Scanner(System.in);
-            int pos = sc.nextInt()-1;
-            sc.close();
+            // Player Aman give one position
+            System.out.println("Player: " + players[turn].getPlayerName() + " give one position");
+            Scanner scn = new Scanner(System.in);
+            int pos = scn.nextInt()-1;
 
-            int sz=board.size;
+            int sz = board.size;
+
             int row = pos/sz;
             int col = pos%sz;
 
-            if(row<0 || row>=sz || col<0 || col>=sz) {
+            // Checking for invalid position
+            if (row <0 || row >= sz || col < 0 || col >= sz) {
                 System.out.println("Invalid position");
                 continue;
             }
 
-            if(board.matrix[row][col] !='-') {
-                System.out.println("Postion already occupied");
+            // Checking for vaccant position
+            if (board.matrix[row][col] != '-') {
+                System.out.println("Position already occupied");
+                continue;
             }
-            
-            return pos;
 
+            return pos;
         }
-    
     }
 
-    public boolean checkCombination() {
+    public boolean checkCombinations() {
         int sz = board.size;
-        
-        for(int i=0;i<sz;i++){
-            StringBuilder sp = new StringBuilder();
-            for(int j=0;j<sz;j++){
-                sp.append(board.matrix[i][j]);
- 
-            }
-            String p1 = sp.toString();
-            if(p1.equals(zero) || p1.equals(cross)  ) {
-                return true;
-            }
-        }
-        
-        
+
+        // Rowise 
         for(int i=0;i<sz;i++) {
             StringBuilder sb = new StringBuilder();
             for(int j=0;j<sz;j++) {
                 sb.append(board.matrix[i][j]);
             }
-            String p = sb.toString();
-            if(p.equals(zero) || p.equals(cross)  ) {
+            
+            String pattern = sb.toString();
+            if (pattern.equals(zero) || pattern.equals(cross)) {
                 return true;
             }
         }
-        
+
+        // Columwise
+        for(int i=0;i<sz;i++) {
+            StringBuilder sb = new StringBuilder();
+            for(int j=0;j<sz;j++) {
+                sb.append(board.matrix[j][i]);
+            }
+            String pattern = sb.toString();
+            if (pattern.equals(zero) || pattern.equals(cross)) {
+                return true;
+            }
+        }
+
+        // Diagonal
+
         int i=0,j=0;
-        StringBuilder ss = new StringBuilder();
-        while(i<sz) {
-            ss.append(board.matrix[i][j]);
+        StringBuilder sb = new StringBuilder();
+
+        while (i<sz) {
+            sb.append(board.matrix[i][j]);
             i++;
             j++;
         }
-        String pat = ss.toString();
-        if(pat.equals(zero) || pat.equals(cross)  ) {
+
+        String pattern = sb.toString();
+        if (pattern.equals(zero) || pattern.equals(cross)) {
             return true;
         }
-        
+
+        // Anti Diagonal
 
         i=0;
         j=sz-1;
-        ss = new StringBuilder();
-        while(i<sz) {
-            ss.append(board.matrix[i][j]);
+        sb = new StringBuilder();
+
+        while (i<sz) {
+            sb.append(board.matrix[i][j]);
             i++;
             j--;
         }
-        pat = ss.toString();
-        if(pat.equals(zero) || pat.equals(cross)  ) {
+
+        pattern = sb.toString();
+        if (pattern.equals(zero) || pattern.equals(cross)) {
             return true;
         }
-
 
         return false;
 
     }
+
 }
 
 
