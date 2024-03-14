f0311874e4bccc24b640530c0f3aa94f87560075
Deepikagowtham <126891628+Deepikagowtham@users.noreply.github.com>
2024-03-14 19:11:27 +0530

    Update App.java

--- project/codes/App.java
+++ project/codes/App.java
@@ -1,23 +1,23 @@
 import board.Board;
-import player.Player;
 import game.Game;
-
+import player.Player;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("First Project-> Tic Tac Toe");
+   public static void main(String[] args) throws Exception {
+      System.out.println("First Project -> Tic Tac Toe");
 
-        Board b = new Board(4,'-');
-        //b.setDefaultSymbol('-');
-        b.printBoardConfig();
-        Player p1 = new Player();
-        Player p2 = new Player();
-        p1.setPlayerDetails("deepi", 'O', "Erode", 638155355, "deepi@gmail.com", 19);
-        p2.setPlayerDetails("nagul", 'X', "salem", 1431431438, "nagul@gmail.com", 20);
-        p1.getPlayerDetails();
-        p2.getPlayerDetails();
+      Board b = new Board(3, '-');
 
-        Game game = new game (new Player[] (p1 ,p2), b);
-        game.play();
-    }
+      Player p1 = new Player();
+      p1.setPlayerNameAndSymbol("Deepi", 'X');
+      p1.getPlayerNameAndSymbol();
+
+      Player p2 = new Player();
+      p2.setPlayerNameAndSymbol("Nagul", 'O');
+      p2.getPlayerNameAndSymbol();
+
+      Game game  = new Game(new Player[] {p1, p2}, b);
+      game.play();
+
+   }
 }
